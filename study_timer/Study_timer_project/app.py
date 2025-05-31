be73cd4e425ecc5e12241761d1fac3f12db5afe2
parth34patil <parrthhp67@gmail.com>
2025-05-31 17:34:33 +0530

    adding comment in code

--- study_timer/Study_timer_project/app.py
+++ study_timer/Study_timer_project/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+# Import Flask and render_template to serve HTML pages
+from flask import Flask, render_template # type: ignore
 
+# Create an instance of the Flask application
 app = Flask(__name__)
 
+# Define the home route (default URL "/")
 @app.route('/')
 def home():
+    # Render the "clock.html" file from the templates folder
     return render_template("clock.html")
 
+# Run the application if this script is executed directly
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)  # Enable debug mode for easier development
